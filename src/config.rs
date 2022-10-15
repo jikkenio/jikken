90a23323332906ee5160eeda802ac56d936731c4
Louis Russo <louis@jikken.io>
2022-10-15 09:39:38 -0500

    combine config files into single src

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,14 @@
-use super::config_settings;
 use serde::Deserialize;
 use std::collections::BTreeMap;
 
 #[derive(Deserialize, Clone, Debug)]
 pub struct Config {
-    pub settings: Option<config_settings::Settings>,
+    pub settings: Option<Settings>,
     pub globals: Option<BTreeMap<String, String>>,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Settings {
+    pub continue_on_failure: Option<bool>,
 }
