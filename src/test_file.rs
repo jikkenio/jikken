484ce0a53101642193ed7197db7e6cd49ef41d75
Louis Russo <louis@jikken.io>
2022-12-21 09:06:22 -0600

    implement initial dependency graph ordering of test execution

--- src/test_file.rs
+++ src/test_file.rs
@@ -1,13 +1,15 @@
 use crate::test_definition::{HttpVerb, Modifier, VariableTypes};
 use serde::{Deserialize, Serialize};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
 pub struct UnvalidatedHttpHeader {
     pub header: String,
     pub value: String,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
 pub struct UnvalidatedHttpParameter {
     pub param: String,
     pub value: String,
@@ -20,6 +22,15 @@ pub struct UnvalidatedRequest {
     pub params: Option<Vec<UnvalidatedHttpParameter>>,
     pub headers: Option<Vec<UnvalidatedHttpHeader>>,
     pub body: Option<serde_json::Value>,
+}
+
+impl Hash for UnvalidatedRequest {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.method.hash(state);
+        self.url.hash(state);
+        self.params.hash(state);
+        self.headers.hash(state);
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -36,6 +47,19 @@ pub struct UnvalidatedCompareRequest {
     pub body: Option<serde_json::Value>,
 }
 
+impl Hash for UnvalidatedCompareRequest {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.method.hash(state);
+        self.url.hash(state);
+        self.params.hash(state);
+        self.add_params.hash(state);
+        self.ignore_params.hash(state);
+        self.headers.hash(state);
+        self.add_headers.hash(state);
+        self.ignore_headers.hash(state);
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct UnvalidatedResponse {
     pub status: Option<u16>,
@@ -44,7 +68,15 @@ pub struct UnvalidatedResponse {
     pub ignore: Option<Vec<String>>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+impl Hash for UnvalidatedResponse {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.status.hash(state);
+        self.headers.hash(state);
+        self.ignore.hash(state);
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
 #[serde(rename_all = "camelCase")]
 pub struct UnvalidatedVariable {
     pub name: String,
@@ -57,14 +89,23 @@ pub struct UnvalidatedVariable {
     index: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
 pub struct UnvalidatedTest {
     pub name: Option<String>,
     pub id: Option<String>,
+    pub requires: Option<String>,
     pub tags: Option<String>,
     pub iterate: Option<u32>,
     pub request: UnvalidatedRequest,
     pub compare: Option<UnvalidatedCompareRequest>,
     pub response: Option<UnvalidatedResponse>,
     pub variables: Option<Vec<UnvalidatedVariable>>,
+}
+
+impl UnvalidatedTest {
+    pub fn generate_id(&self) -> String {
+        let mut s = DefaultHasher::new();
+        self.hash(&mut s);
+        format!("{}", s.finish())
+    }
 }
