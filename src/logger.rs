b4a3e1d33117994081f252194a211c46ff2e6acb
Louis Russo <louis@jikken.io>
2023-02-06 08:58:31 -0600

    clean-up logging adjusting verbose and trace messaging

--- src/logger.rs
+++ src/logger.rs
@@ -14,7 +14,7 @@ impl Log for SimpleLogger {
         if self.disabled {
             return;
         }
-        
+
         if !self.enabled(record.metadata()) {
             return;
         }
@@ -22,8 +22,14 @@ impl Log for SimpleLogger {
         match record.level() {
             Level::Info => {
                 print!("{}", record.args());
-            },
-            _ => println!("{}", record.args())
+            }
+            Level::Warn => {
+                println!("\x1b[33m{}\x1b[0m", record.args());
+            }
+            Level::Error => {
+                println!("\x1b[31m{}\x1b[0m", record.args());
+            }
+            _ => println!("{}", record.args()),
         }
     }
 
