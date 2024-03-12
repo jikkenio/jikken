65f8706af82b8743f6072d6b75fa82a77d9e7cd7
Darius Salemizadeh <darius.salemizadeh@gmail.com>
2024-03-11 20:41:19 -0500

    Ignore file support

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,12 @@ use log::{debug, error, info, Level, LevelFilter};
 use logger::SimpleLogger;
 use serde::{Deserialize, Serialize};
 use std::error::Error;
+use std::ffi::OsStr;
+use std::path::Path;
 use tokio::fs;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+const IGNORE_FILE: &str = ".jikkenignore";
 
 pub enum TagMode {
     AND,
@@ -197,8 +200,40 @@ async fn search_directory(
     Ok(ret)
 }
 
+async fn get_ignore_files(ignore_file: &std::path::Path) -> Vec<String> {
+    tokio::fs::read_to_string(ignore_file)
+        .await
+        .ok()
+        .map(|s| {
+            s.split('\n')
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
+        })
+        .unwrap_or_default()
+}
+
+fn ignore_matches<'a>(ignore_pattern: &'a str, file: &'a str) -> bool {
+    let ignore_pattern_path = std::path::Path::new(ignore_pattern);
+    let file_path = std::path::Path::new(file);
+
+    let filename_extractor = |s: &'a Path| -> &'a OsStr { s.file_stem().unwrap_or_default() };
+    let dirname_extractor =
+        |s: &'a Path| -> &'a str { s.parent().and_then(|s| s.to_str()).unwrap_or_default() };
+
+    if ignore_pattern_path.is_file() {
+        return filename_extractor(file_path) == filename_extractor(ignore_pattern_path);
+    } else if ignore_pattern_path.is_dir() {
+        return dirname_extractor(file_path) == ignore_pattern_path.to_str().unwrap_or_default();
+    }
+
+    glob::Pattern::new(ignore_pattern)
+        .map(|p| p.matches_path(file_path))
+        .unwrap_or_default()
+}
+
 async fn get_files(
     paths: Vec<String>,
+    ignore_file: &std::path::Path,
     recursive: bool,
 ) -> Result<Vec<String>, Box<dyn Error + Send + Sync>> {
     let mut results: Vec<String> = Vec::new();
@@ -230,6 +265,16 @@ async fn get_files(
         }
     }
 
+    let ignore_patterns = get_ignore_files(ignore_file).await;
+    results = results
+        .into_iter()
+        .filter(|f| {
+            !ignore_patterns
+                .iter()
+                .any(|ignore| ignore_matches(ignore.as_str(), f.as_str()))
+        })
+        .collect();
+
     for r in results.clone() {
         debug!("file: {}", r);
     }
@@ -256,7 +301,7 @@ async fn run_tests(
 
     let cli_tag_mode = if tags_or { TagMode::OR } else { TagMode::AND };
     let config = config::get_config(config_file).await;
-    let files = get_files(cli_paths, recursive).await?;
+    let files = get_files(cli_paths, std::path::Path::new(IGNORE_FILE), recursive).await?;
     let plurality_policy = |count: usize| match count {
         1 => "",
         _ => "s",
@@ -407,76 +452,189 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 //mod file_capture{
 #[cfg(test)]
 mod tests {
+    use std::io::Write;
+
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
     use {std::fs::File, tempfile::tempdir};
 
+    struct DirectoryFixture {
+        pub temp_dir: tempfile::TempDir,
+    }
+
+    //todo: support arbitrary depth
+    impl DirectoryFixture {
+        fn new(file_names: &[&str]) -> DirectoryFixture {
+            let tmp_dir = tempdir().unwrap();
+            let tmp_path = tmp_dir.path();
+            let _: Vec<std::fs::File> = file_names
+                .into_iter()
+                .map(|f| File::create(tmp_path.join(f)).unwrap())
+                .collect();
+
+            DirectoryFixture { temp_dir: tmp_dir }
+        }
+
+        fn path_string(&self) -> String {
+            self.temp_dir.path().to_str().unwrap().to_string()
+        }
+
+        fn create_ignore_file(&self, file_names: &[&str]) {
+            let ignore_file = self.temp_dir.path().join(".jikkenignore");
+            let str_to_write = file_names.join("\n");
+            _ = File::create(&ignore_file)
+                .unwrap()
+                .write(str_to_write.as_bytes());
+        }
+
+        async fn get_files(&self, recursive: bool, path: Option<String>) -> Vec<String> {
+            get_files(
+                vec![path.unwrap_or(self.path_string())],
+                self.temp_dir.path().join(".jikkenignore").as_path(),
+                recursive,
+            )
+            .await
+            .unwrap()
+        }
+    }
+
     #[tokio::test]
-    async fn get_files_with_one_level_of_depth_recursively() {
-        let tmp_dir = tempdir().unwrap();
-        let tmp_path = tmp_dir.path();
-        let tmp_path_str = tmp_path.to_str().unwrap();
-        {
-            //Begin Scope
-            let _: Vec<std::fs::File> = vec![
+    async fn get_files_with_glob_ignore() {
+        let dir_fixture = DirectoryFixture::new(
+            vec![
                 "random_file",
                 "my_test.jkt",
                 "something_else",
                 "my_test_2.jkt",
             ]
-            .iter()
-            .map(|f| File::create(tmp_path.join(f)).unwrap())
-            .collect();
-            let found_files = get_files(vec![String::from(tmp_path_str)], true).await;
-            assert_eq!(2, found_files.unwrap().len());
-        } //End Scope
-        _ = tmp_dir.close();
+            .as_slice(),
+        );
+
+        dir_fixture.create_ignore_file(
+            vec![dir_fixture
+                .temp_dir
+                .path()
+                .join("my_test*")
+                .to_str()
+                .unwrap_or_default()]
+            .as_slice(),
+        );
+
+        assert_eq!(0, dir_fixture.get_files(true, None).await.len());
+    }
+
+    #[tokio::test]
+    async fn get_files_with_dir_ignore() {
+        let dir_fixture = DirectoryFixture::new(
+            vec![
+                "random_file",
+                "my_test.jkt",
+                "something_else",
+                "my_test_2.jkt",
+            ]
+            .as_slice(),
+        );
+
+        dir_fixture.create_ignore_file(
+            vec![dir_fixture.temp_dir.path().to_str().unwrap_or_default()].as_slice(),
+        );
+
+        assert_eq!(0, dir_fixture.get_files(true, None).await.len());
+    }
+
+    #[tokio::test]
+    async fn get_files_with_filename_ignore() {
+        let dir_fixture = DirectoryFixture::new(
+            vec![
+                "random_file",
+                "my_test.jkt",
+                "something_else",
+                "my_test_2.jkt",
+            ]
+            .as_slice(),
+        );
+
+        dir_fixture.create_ignore_file(
+            vec![
+                dir_fixture
+                    .temp_dir
+                    .path()
+                    .join("my_test.jkt")
+                    .to_str()
+                    .unwrap_or_default(),
+                dir_fixture
+                    .temp_dir
+                    .path()
+                    .join("my_test_2.jkt")
+                    .to_str()
+                    .unwrap_or_default(),
+            ]
+            .as_slice(),
+        );
+
+        assert_eq!(0, dir_fixture.get_files(true, None).await.len());
+    }
+
+    #[tokio::test]
+    async fn get_files_with_one_level_of_depth_recursively() {
+        let dir_fixture = DirectoryFixture::new(
+            vec![
+                "random_file",
+                "my_test.jkt",
+                "something_else",
+                "my_test_2.jkt",
+            ]
+            .as_slice(),
+        );
+
+        assert_eq!(2, dir_fixture.get_files(true, None).await.len());
     }
 
     #[tokio::test]
     async fn get_files_with_one_level_of_depth_non_recursively() {
-        let tmp_dir = tempdir().unwrap();
-        let tmp_path = tmp_dir.path();
-        let tmp_path_str = tmp_path.to_str().unwrap();
-        {
-            //Begin Scope
-            let _: Vec<std::fs::File> = vec![
+        let dir_fixture = DirectoryFixture::new(
+            vec![
                 "random_file",
                 "my_test.jkt",
                 "something_else",
                 "my_test_2.jkt",
             ]
-            .iter()
-            .map(|f| File::create(tmp_path.join(f)).unwrap())
-            .collect();
-            let found_files = get_files(vec![String::from(tmp_path_str)], false).await;
-            assert_eq!(2, found_files.unwrap().len());
-        } //End Scope
-        _ = tmp_dir.close();
+            .as_slice(),
+        );
+
+        assert_eq!(2, dir_fixture.get_files(false, None).await.len());
     }
 
     #[tokio::test]
     async fn get_files_with_one_level_of_depth_non_recursively_globbing() {
-        let tmp_dir = tempdir().unwrap();
-        let tmp_path = tmp_dir.path();
-        let glob_path = tmp_path.join("*_2*");
-        let glob_path_str = glob_path.to_str().unwrap();
-
-        {
-            //Begin Scope
-            let _: Vec<std::fs::File> = vec![
+        let dir_fixture = DirectoryFixture::new(
+            vec![
                 "random_file",
                 "my_test.jkt",
                 "something_else",
                 "my_test_2.jkt",
             ]
-            .iter()
-            .map(|f| File::create(tmp_path.join(f)).unwrap())
-            .collect();
-            let found_files = get_files(vec![String::from(glob_path_str)], false).await;
-            assert_eq!(1, found_files.unwrap().len());
-        } //End Scope
-        _ = tmp_dir.close();
+            .as_slice(),
+        );
+
+        assert_eq!(
+            1,
+            dir_fixture
+                .get_files(
+                    false,
+                    Some(
+                        dir_fixture
+                            .temp_dir
+                            .path()
+                            .join("*_2*")
+                            .to_str()
+                            .unwrap_or_default()
+                            .to_string()
+                    )
+                )
+                .await
+                .len()
+        );
     }
 
     #[tokio::test]
@@ -496,7 +654,12 @@ mod tests {
             .iter()
             .map(|f| File::create(tmp_path.join(f)).unwrap())
             .collect();
-            let found_files = get_files(vec![String::from(glob_path_str)], true).await;
+            let found_files = get_files(
+                vec![String::from(glob_path_str)],
+                std::path::Path::new(".does_not_exist"),
+                true,
+            )
+            .await;
             assert_eq!(1, found_files.unwrap().len());
         } //End Scope
         _ = tmp_dir.close();
