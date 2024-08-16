de553c35d2ab8d2436be9939fb452af3fc9b5f91
Darius Salemizadeh <darius@jikken.io>
2024-08-16 00:15:11 -0500

    JK-503 -- DateTime modifiers generation issue. Also, improve flexibility of variable parsing by using serde alias

--- src/test/variable.rs
+++ src/test/variable.rs
@@ -1,10 +1,37 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::hash::Hash;
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash, PartialOrd)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Modifier {
     pub operation: String,
-    pub value: String,
+    pub value: Value,
     pub unit: String,
+}
+
+impl Hash for Modifier {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        serde_json::to_string(self).unwrap().hash(state);
+    }
+}
+
+impl Modifier {
+    //We need an enum for modifier
+    pub fn get_inverse(&self) -> Modifier {
+        match self.operation.as_str() {
+            "add" => Self {
+                operation: "subtract".to_string(),
+                value: self.value.clone(),
+                unit: self.unit.clone(),
+            },
+            "subtract" => Self {
+                operation: "add".to_string(),
+                value: self.value.clone(),
+                unit: self.unit.clone(),
+            },
+            _ => self.clone(),
+        }
+    }
 }
 
 pub fn parse_source_path(path: &str) -> String {
