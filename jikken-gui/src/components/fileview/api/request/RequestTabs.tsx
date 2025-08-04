91d7d3b09bbb4e801c95ce760230b1412d2cd761
Cameron Besse <cameron@jikken.io>
2025-08-04 14:57:49 -0500

    Revert "JK-592: Change no-count tab indicator to dot"
    This reverts commit c3f22cad1a5e4794fee01c359998cdb549d588d0.

--- jikken-gui/src/components/fileview/api/request/RequestTabs.tsx
+++ jikken-gui/src/components/fileview/api/request/RequestTabs.tsx
@@ -63,16 +63,11 @@ export const RequestTabs = () => {
                                             class="ml-0.5 my-auto inline-block"
                                             classList={{
                                                 "text-indigo-500": tab.index === layoutState().requestTabIndex,
-                                                "text-neutral-400 group-hover:text-neutral-300": tab.index !== layoutState().requestTabIndex,
+                                                "text-neutral-300 group-hover:text-neutral-300": tab.index !== layoutState().requestTabIndex,
                                             }}
                                         >
-                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                width="16"
-                                                height="16"
-                                                fill="currentColor"
-                                                class="bi bi-dot"
-                                                viewBox="0 0 16 16">
-                                                <path d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
                                             </svg>
                                         </span>
                                     </Show>
